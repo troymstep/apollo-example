d392f8aa158f3f0e3d21e5dd8a112d71533617a5
Troy Stephenson <tms7644@rit.edu>
2023-05-01 17:27:25 -0400

    added useQuery

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from '../components';
-import { gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 
 const TRACKS = gql`
 query GetTracks {
@@ -24,7 +24,13 @@ query GetTracks {
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return 'loading...';
+
+  if (error) return `ERROR! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
